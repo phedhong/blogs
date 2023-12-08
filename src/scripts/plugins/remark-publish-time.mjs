8d8d41cbdbcd2eac2365ec578a0727745eb8ff86
ed <42587998+phedhong@users.noreply.github.com>
2023-12-08 13:50:31 -0600

    Update PubDate Time Logical

--- src/scripts/plugins/remark-publish-time.mjs
+++ src/scripts/plugins/remark-publish-time.mjs
@@ -5,7 +5,7 @@ export default function remarkPublishedTime() {
   return function (tree, file) {
     const filepath = file.history[0];
 
-    if (file.data.astro.frontmatter.pubDate) {
+    if (file.data.astro.frontmatter?.draft === false) {
       const result = execSync(`git log -1 --pretty="format:%cI" ${filepath}`);
       file.data.astro.frontmatter.pubDate = result.toString();
     }
